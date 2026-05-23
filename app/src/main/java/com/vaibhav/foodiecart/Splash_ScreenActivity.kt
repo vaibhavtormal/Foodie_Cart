102f8ae4c98e37f4e6661d1c2cd8b1b6b7d05381
vaibhavrameshwartormal <vaibhavtormal2020@gmail.com>
2026-05-23 19:32:27 +0530

    Certainly! Here's a concise message you can add to your GitHub repository for your food online delivery app:
    Welcome to Foodie Cart App!
    Thank you for visiting our GitHub repository for FoodOnlineDeliveryApp - your go-to solution for convenient and delicious food delivery!
    
    About FoodOnlineDeliveryApp
    FoodOnlineDeliveryApp is a cutting-edge online platform designed to streamline the food ordering and delivery process. With a user-friendly interface and robust functionality, our app aims to enhance the dining experience for customers and empower restaurants to reach a wider audience.
    
    Contributing to FoodOnlineDeliveryApp
    We welcome contributions from developers, designers, and enthusiasts who are passionate about improving the online food delivery experience. Whether you're interested in fixing bugs, adding new features, improving performance, or enhancing documentation, your contributions are invaluable to our project's success.
    
    Signed-off-by: vaibhavrameshwartormal <vaibhavtormal2020@gmail.com>

--- app/src/main/java/com/vaibhav/foodiecart/Splash_ScreenActivity.kt
+++ app/src/main/java/com/vaibhav/foodiecart/Splash_ScreenActivity.kt
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
+
 class Splash_ScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         Handler(Looper.getMainLooper()).postDelayed({
-            //if you have any error replace mainactivity::class. java to startactivty::class java
-            val intent = Intent(this, LoginActivity::class.java)
+            val destination = if (FirebaseAuth.getInstance().currentUser != null) {
+                MainActivity::class.java
+            } else {
+                LoginActivity::class.java
+            }
+
+            val intent = Intent(this, destination)
             startActivity(intent)
             finish()
         }, 3000)
